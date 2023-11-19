91d13e8a9a9cf6a4620b0b1fd1dc45556f64114f
AndreHGN <andrehidekinishimura@gmail.com>
2023-11-19 18:01:51 -0300

    Export PNG

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, Fragment, useEffect, useState } from 'react';
-import { ApollonEditor, ApollonMode, ApollonOptions, Locale, UMLDiagramType } from '@ls1intum/apollon';
+import { ApollonEditor, ApollonMode, ApollonOptions, Locale, UMLDiagramType, SVG } from '@ls1intum/apollon';
 import './App.css';
 import { ApollonEditorComponent } from './components/apollon-editor/apollon-editor.component';
 
@@ -54,8 +54,27 @@ function App() {
     document.body.removeChild(link);
   }
 
+  const exportPng = () => {
+    editor?.exportAsSVG().then((renderedSvg) => {
+      return convertRenderedSVGToPNG(renderedSvg, true);
+    }).then((blob) => {
+      const objectUrl = URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+  
+      link.setAttribute('href', objectUrl);
+      link.setAttribute('download', 'teste');
+      link.style.display = 'none';
+  
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    })
+  }
+
   return (
     <Fragment>
+      <button onClick={exportPng}>Baixar PNG</button>
       <button onClick={handleDownloadJSON}>Baixar JSON</button>
       <input type="file" onChange={handleImportFile} value={undefined}/>
       <ApollonEditorComponent options={options} setEditor={setEditor} setDiagram={setDiagram}/>
@@ -64,3 +83,44 @@ function App() {
 }
 
 export default App;
+
+const convertRenderedSVGToPNG = (renderedSVG: SVG, whiteBackground: boolean): Promise<Blob> => {
+  return new Promise((resolve, reject) => {
+    const { width, height } = renderedSVG.clip;
+
+    const blob = new Blob([renderedSVG.svg], { type: 'image/svg+xml' });
+    const blobUrl = URL.createObjectURL(blob);
+
+    const image = new Image();
+    image.width = width;
+    image.height = height;
+    image.src = blobUrl;
+
+    image.onload = () => {
+      let canvas: HTMLCanvasElement;
+      canvas = document.createElement('canvas');
+      canvas.style.width = `${width}px`;
+      canvas.style.height = `${height}px`;
+
+      const scale = 1.5;
+      canvas.width = width * scale;
+      canvas.height = height * scale;
+
+      const context = canvas.getContext('2d')!;
+
+      if (whiteBackground) {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+      }
+
+      context.scale(scale, scale);
+      context.drawImage(image, 0, 0);
+
+      canvas.toBlob(resolve as BlobCallback);
+    };
+
+    image.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
